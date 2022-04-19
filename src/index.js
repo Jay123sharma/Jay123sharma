d99d61b5c1d4af62ddd3fa1f39eb8c866b4c7a6b
jay123 <jay175147@gmail.com>
2022-04-20 02:05:01 +0530

    trfd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment=require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +14,12 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
-
+app.use(
+    function (req,res,next){
+        console.log(moment().format('YYYY-MM-DD HH:mm:ss'),',',req.ip,',',req.path);
+        next();
+    }
+);
 app.use('/', route);
 
 
