a87a77eb3facfdffbf65333defccb235b24baaa3
jay123 <jay175147@gmail.com>
2022-04-25 23:37:15 +0530

    assignment/axios-promise

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://jaysh_db:LsPKC8Fxm42z2LaS@cluster0.g2uni.mongodb.net/jaysh_db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
